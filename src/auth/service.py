e8a560dcf92919fc603f267ff6a827a3c76163ac
gorik0 <delavarus@gmail.com>
2025-04-23 22:33:21 +0300

    user_service

--- src/auth/service.py
+++ src/auth/service.py
@@ -2,7 +2,8 @@
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 from src.auth.models import User
-
+from src.auth.schemas import UserCreateModel
+from utils import generate_hash
 
 
 class UserService :
@@ -14,4 +15,12 @@ class UserService :
     async def user_exists(self, email:str, session:AsyncSession)-> bool :
         user = self.get_user_by_email(email,session)
         return user is not None
-
+    async def create_user(self, user_create:UserCreateModel, session:AsyncSession)-> bool :
+      user_data = user_create.model_dump()
+      new_user = User(
+          **user_create
+      )
+      new_user.password_hash = generate_hash(user_data['password'])
+      session.add(new_user)
+      await session.commit()
+      return new_user
