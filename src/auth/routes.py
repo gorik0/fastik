c483e6803e313e04d7f74ad2e3343b60f476b8f4
gorik0 <delavarus@gmail.com>
2025-04-24 14:24:32 +0300

    auth_token

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,7 +1,8 @@
+from datetime import timedelta
 from fastapi import APIRouter,Depends,status
 
 
-from src.auth.schemas import User, UserCreateModel
+from src.auth.schemas import User, UserCreateModel, UserLoginModel
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -9,8 +10,12 @@ from src.auth.service import UserService
 from src.db.main import get_session
 from fastapi.exceptions import  HTTPException
 
+from utils import create_token, verify_pass
+
+from fastapi.responses import JSONResponse
 auth_router = APIRouter()
 
+REFRESH_EXPIRY_D =2
 
 auth_service = UserService()
 
@@ -27,3 +32,49 @@ async def create_user(user_data :UserCreateModel,session:AsyncSession = Depends(
     else:
         new_user = await auth_service.create_user(user_data,session)
         return new_user
+@auth_router.post('/login')
+async def create_user(login_data  :UserLoginModel,session:AsyncSession = Depends(get_session)):
+    
+    user  =await auth_service.get_user_by_email(login_data.email,session)
+    if user:
+       pass_hash  = verify_pass(login_data.password, user.password_hash)
+       if pass_hash:
+        access_token = create_token(
+           user_data={
+              'email':user.email,
+              'uid':user.uid,
+           },
+
+        )
+        refresh_token = create_token(
+           user_data={
+              'email':user.email,
+              'uid':user.uid,
+           },
+           refresh= True,
+           expiry=timedelta(days=REFRESH_EXPIRY_D)
+
+        ) 
+        return JSONResponse(
+           content={
+              "message":"Succe login",
+              "refresh_token ":refresh_token,
+              "refresh_token ":refresh_token,
+              "user_data ":{
+                 "email":user.email,
+                 "uid":user.uid,
+              },
+           }
+        )
+       else:
+          raise HTTPException(
+             detail="invalid pass!!! .. . ..",
+             status_code=status.HTTP_403_FORBIDDEN
+          )
+
+    else:
+        raise HTTPException(
+           detail="no user with such an email",
+           status_code=status.HTTP_404_NOT_FOUND
+        )
+
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -7,6 +7,11 @@ class UserCreateModel (BaseModel):
     username :str = Field(max_length=8)
     email :str = Field(max_length=40)
     password :str = Field(min_length=6)
+
+
+class UserLoginModel (BaseModel):
+    email :str = Field(max_length=40)
+    password :str = Field(min_length=6)
 class User(BaseModel):
     uid:uuid.UUID 
     username:str
